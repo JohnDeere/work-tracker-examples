a0683905ed1b12dcc3e16261fa425e72abc849ba
Ryan Bergman <bergmanpatrickr@johndeere.com>
2022-08-27 09:11:44 -0500

    upgrade and remove groovy logback config

--- java-example/src/test/java/com/deere/example/WorkTrackerContextListenerTest.java
+++ java-example/src/test/java/com/deere/example/WorkTrackerContextListenerTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2019 Deere & Company
+ * Copyright 2022 Deere & Company
  *
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
